4c4fd2c74245fa9ac0b2a07a40c34f00195817e8
NataliVod <n3824@yandex.ru>
2023-03-12 01:02:33 +0300

    stage2

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,7 +1,32 @@
 package hexlet.code;
+import picocli.CommandLine;
+import picocli.CommandLine.Command;
+import picocli.CommandLine.Option;
+import picocli.CommandLine.Parameters;
 
-public class App {
+import java.io.File;
+import java.math.BigInteger;
+import java.nio.file.Files;
+import java.security.MessageDigest;
+import java.util.concurrent.Callable;
+@Command(name = "gendiff",
+        mixinStandardHelpOptions = true,
+        version = "1.0",
+        description = "Compares two configuration files and shows a difference.")
+public class App implements Callable<Integer> {
+    @Option(names = {"-h", "--help"},description = "Show this help message and exit.")
+    private String help;
+
+    @Option(names = {"-v", "--version"}, description = "Print version information and exit.")
+    private String version;
+
+    @Override
+    public Integer call() throws Exception {
+        return 0;
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int exitCode = new CommandLine(new App()).execute(args);
+        System.exit(exitCode);
     }
 }
---
+++ app/src/main/java/hexlet/code/Differ.java
@@ -0,0 +1,7 @@
+package hexlet.code;
+
+public class Differ {
+    static void generate(){
+
+    }
+}
